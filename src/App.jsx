0a0ec5fc1c5184994b749e4ae36b5773bb2c3b8c
RegioProfessionals <richardvanbeem@live.nl>
2025-01-25 12:06:15 +0100

    Opdracht 1 afgerond

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import './App.css';
-import {countSoldItems} from "../utils/countitems.js";
+import {countSoldItems} from "./utils/countitems.js";
+import {countBoughtItems} from "./utils/countitems.js";
 
 function App() {
 
-  countSoldItems();
+  let numberOfBoughtTVs = countBoughtItems();
+  let numberOfSoldTVs = countSoldItems();
+  let stillToSell = countBoughtItems()-countSoldItems();
+  console.log(numberOfSoldTVs);
+  console.log(numberOfBoughtTVs);
+  console.log(stillToSell);
   return (
-    <h1>test</h1>
+      <>
+          <h1>Aantal verkochte producten</h1>
+          <article className="sold">
+              <p>{numberOfSoldTVs}</p>
+          </article>
+          <h1>Aantal ingekochte producten</h1>
+          <article className="bought">
+              <p>{numberOfBoughtTVs}</p>
+          </article>
+          <h1>Voorraad</h1>
+          <article className="stock">
+              <p>{stillToSell}</p>
+          </article>
+      </>
 
   )
 }
